2631ff40da300b424fec23d22edc7b0e2a739f1c
acer <elmurodpro@gmail.com>
2025-05-12 21:46:02 +0500

    update

--- keyboards.py
+++ keyboards.py
@@ -1,6 +1,6 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 
-web_app = WebAppInfo(url="https://github.com/Elmurod31/Elmurod_burger.github.io")
+web_app = WebAppInfo(url="https://elmurod31.github.io/Elmurod_burger.github.io/")
 web_app_btn = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text="Web App", web_app=web_app)]
 ])
